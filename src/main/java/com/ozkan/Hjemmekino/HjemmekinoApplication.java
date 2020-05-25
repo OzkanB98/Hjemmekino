16f992f9af1fe26f0f1c33e965c81a40b22d8bd9
Ozkan-Bicak <32769137+Ozkan-Bicak@users.noreply.github.com>
2020-05-26 01:54:39 +0200

    Prosjektfiler

---
+++ src/main/java/com/ozkan/Hjemmekino/HjemmekinoApplication.java
@@ -0,0 +1,13 @@
+package com.ozkan.Hjemmekino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HjemmekinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HjemmekinoApplication.class, args);
+	}
+
+}
